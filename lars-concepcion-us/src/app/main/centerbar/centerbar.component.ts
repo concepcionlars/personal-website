baa4784e9a5e90e612084d70bc5ae6ed53628745
concepcionlars <concepcionlars1997@gmail.com>
2018-11-16 16:34:00 +0800

    Add files and Directory centerbar and sidebar

---
+++ lars-concepcion-us/src/app/main/centerbar/centerbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centerbar',
+  templateUrl: './centerbar.component.html',
+  styleUrls: ['./centerbar.component.css']
+})
+export class CenterbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
